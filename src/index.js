095159a1af6c5e7e8c1f64f34495cbbefcb7ec66
Utkarsh <utk1298@gmail.com>
2026-02-17 12:31:47 +0530

    Make the button functional

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import "./styles.css";
 import { initialLoader } from "./home.js";
+import { menuPage } from "./menu.js";
+import { aboutPage } from "./about.js";
 
-initialLoader();
+document.addEventListener('click',(e)=>{
+    const content = document.getElementById("content");
+    content.innerHTML="";
+    if(e.target.innerText === "Home"){
+        initialLoader();
+    }else if(e.target.innerText === "Menu"){
+        menuPage();
+    }else if(e.target.innerHTML === "About"){
+        aboutPage();
+    }
+})
 console.log("is it working!!");
